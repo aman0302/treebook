72566f23fbb42f6c2522801cacc7f76779876922
Aman Gupta <aman.vice@gmail.com>
2014-12-07 15:05:40 +0530

    user and micropost status established

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141204155717) do
+ActiveRecord::Schema.define(version: 20141207082546) do
+
+  create_table "microposts", force: true do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "statuses", force: true do |t|
     t.string   "name"
     t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
